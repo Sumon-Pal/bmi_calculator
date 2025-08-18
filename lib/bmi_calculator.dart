da4fa1f700b53ebe6455378777f37f44ef449ac4
Sumon-Pal <sumon.pal.ice@gmail.com>
2025-08-18 23:41:09 +0600

    Complete the design and functionality

---
+++ lib/bmi_calculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  double height = 175.0;
+  double weight = 80.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI CALCULATOR",
+          style: TextStyle(
+            fontSize: 42,
+            fontWeight: FontWeight.w900,
+            color: Colors.deepOrange,
+            backgroundColor: Colors.white
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 100,),
+            Text('Height:$height',style: TextStyle(fontSize: 34),),
+            const SizedBox(height: 20,),
+            Slider(
+              min: 100,
+                max: 250,
+                divisions: 300,
+                label: "${height.toDouble()}",
+                value: height, onChanged: (value){
+                setState(() {
+                  height = value;
+                });
+            }),
+            const SizedBox(height: 20,),
+            Text('Weight:$weight',style: TextStyle(fontSize: 34),),
+            const SizedBox(height: 20,),
+            Slider(
+              min: 25,
+                max: 425,
+                divisions: 400,
+                label: "${weight.toDouble()}",
+                value: weight, onChanged: (value){
+                setState(() {
+                  weight = value;
+                });
+            }),
+            Text("Your BMI is :${bmi.toStringAsPrecision(2)}",style: TextStyle(fontSize: 28),),
+            Text("Category:$category",style: TextStyle(fontSize: 28),)
+          ],
+        ),
+      ),
+    );
+  }
+  double get bmi {
+    double heightInMeter = height/100;
+    return weight / (heightInMeter*heightInMeter);
+  }
+  String get category {
+    double current = bmi;
+    if(current < 18.5) return "Underweight";
+    if(current > 18.5 && current < 24.9 ) return "Normal Weight";
+    if(current > 25 && current < 29.9 ) return "Class 1 obesity";
+    if(current > 30 && current < 39.9 ) return "Class 2 obesity";
+    return "Class 3 obesity";
+  }
+}
